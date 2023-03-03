4f130e5380ae8d1f0d1090352a249f94a0abd0bf
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-04 00:48:43 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackAndQueue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Problems");
+        }
+    }
+}
