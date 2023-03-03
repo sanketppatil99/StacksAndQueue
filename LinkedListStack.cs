be333e16463ea643aee6ce61dc9e200a1720905f
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-04 00:58:48 +0530

    Ability to create stack of 56,30&70

---
+++ LinkedListStack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        public void Push(int data)
+        {
+            Node top;
+            Node newNode = new Node(data);
+            if (this.top == null)
+            {
+                newNode.next = null;
+            }
+            else
+            {
+                newNode.next = this.top;
+            }
+            this.top = newNode;
+            Console.WriteLine("{0} pushed to stack", data);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Item in the stack");
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
