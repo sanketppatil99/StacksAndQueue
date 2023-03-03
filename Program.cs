72ca5dfc7aaf4f276cb572ea2a9667dd4c584966
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-03-04 01:26:12 +0530

    Ability to dequeue

--- Program.cs
+++ Program.cs
@@ -4,11 +4,16 @@
     {
         public static void Main(string[] args)
         {
-            LinkedListQueue linkedListQueue = new LinkedListQueue();
-            linkedListQueue.Enqueue(56);
-            linkedListQueue.Enqueue(30);
-            linkedListQueue.Enqueue(70);
-            linkedListQueue.Display();
+            LinkedListQueue queue = new LinkedListQueue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+            queue.Display();
+            queue.Dequeu();
+            Console.WriteLine("After Dequeue");
+            queue.Display();
+
         }
     }
 }
